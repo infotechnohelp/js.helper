e7ada24951d07281b208f3aa90ccdbd5a5ad49e0
Philipp <philipp.nikolajev@suomenpt.fi>
2019-01-12 16:56:39 +0200

    clone() and sortByDate() created, 1.1.0

--- src/js.helper.js
+++ src/js.helper.js
@@ -4,3 +4,21 @@ const Helper = function () {
 Helper.prototype.setOption = function (optionTitle, options, defaultOptions) {
     return options[optionTitle] === undefined ? defaultOptions[optionTitle] : options[optionTitle];
 }
+
+Helper.prototype.clone = function(object){
+    return Array.isArray(object) ? Object.assign([], object) : Object.assign({}, object);
+}
+
+Helper.prototype.sortByDate = function (object, desc, keyTitle) {
+    var clone = this.clone(object);
+
+    var desc = (desc === undefined) ? false : desc;
+    var keyTitle = (keyTitle === undefined) ? 'date' : keyTitle;
+
+    return clone.sort(function (a, b) {
+        var first = (desc) ? b : a;
+        var second = (desc) ? a : b;
+
+        return new Date(first.date) - new Date(second.date);
+    });
+}
